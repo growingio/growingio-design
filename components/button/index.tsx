ee51e309ffa1521a791d359d4460dbbcdfeb779d
Pan Jie <fenqi@startdt.com>
2023-11-07 19:30:47 +0800

    chore: initialize repo

---
+++ components/button/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import clsx from "clsx";
+import {
+  default as ArcoButton,
+  ButtonProps as ArcoButtonProps,
+} from "@arco-design/web-react/es/Button";
+
+export type ButtonProps = Omit<ArcoButtonProps, "status"> & {
+  status?: "primary" | "default" | "success" | "warning" | "danger";
+};
+
+const ButtonFunction = (props: ButtonProps, ref: React.Ref<unknown>) => {
+  const { className, status, ...restProps } = props;
+  const cls = clsx({ [`arco-btn-status-${status}`]: !!status }, className);
+  return <ArcoButton ref={ref} {...restProps} className={cls} />;
+};
+
+const ForwardRefButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ButtonFunction
+);
+
+const Button = ForwardRefButton as typeof ForwardRefButton & {
+  Group: typeof ArcoButton.Group;
+};
+
+Button.Group = ArcoButton.Group;
+Button.displayName = "Button";
+
+export default Button;
---
+++ stories/button/Button.stories.tsx
@@ -0,0 +1,340 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { Space } from "@arco-design/web-react";
+import {
+  IconAlignLeft,
+  IconAlignCenter,
+  IconAlignRight,
+  IconPlus,
+  IconLeft,
+  IconRight,
+  IconStar,
+  IconSettings,
+  IconDown,
+} from "@arco-design/web-react/icon";
+import Button from "../../components/button";
+import "../../components/button/style";
+
+const meta: Meta<typeof Button> = {
+  args: {
+    className: undefined,
+    style: undefined,
+    children: "Button Text",
+    htmlType: "button",
+    type: "default",
+    shape: "square",
+    status: undefined,
+    size: undefined,
+    disabled: false,
+    long: false,
+  },
+  argTypes: {
+    status: {
+      control: {
+        type: "select",
+      },
+      description: "按钮状态",
+      defaultValue: "default",
+      options: ["primary", "default"],
+      table: {
+        type: {
+          summary: "primary | default | success | warning | danger",
+        },
+      },
+    },
+  },
+  component: Button,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/ZWk8AwsKxyXRlobAY94Zms/GIO%E6%96%B0%E7%BB%84%E4%BB%B6-%E5%BC%80%E5%8F%91%E8%AF%B4%E6%98%8E?type=design&node-id=4%3A5193&mode=design&t=jsmHyN8goBERVGcA-1",
+    },
+  },
+  title: "通用/Button 按钮",
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const BasicButtons: Story = {
+  name: "基础按钮",
+  render: (args) => (
+    <Space size="large">
+      <Button {...args} type="primary">
+        主要按钮
+      </Button>
+      <Button {...args} type="secondary">
+        默认按钮
+      </Button>
+      <Button {...args} type="dashed">
+        虚线按钮
+      </Button>
+      <Button {...args} type="outline">
+        线框按钮
+      </Button>
+      <Button {...args} type="text">
+        文字按钮
+      </Button>
+    </Space>
+  ),
+};
+
+export const IconButtons: Story = {
+  name: "图标按钮",
+  render: (args) => (
+    <Space size="large">
+      <Button {...args} type="primary" icon={<IconPlus />} />
+      <Button {...args} type="primary" icon={<IconPlus />} />
+      <Button {...args} type="primary">
+        {args.children}
+        <IconPlus />
+      </Button>
+    </Space>
+  ),
+};
+
+export const ButtonShapes: Story = {
+  name: "按钮形状",
+  render: (args) => (
+    <Space size="large">
+      <Button type="primary" icon={<IconPlus />} />
+      <Button shape="circle" type="primary" icon={<IconPlus />} />
+      <Button {...args} type="primary" />
+      <Button {...args} shape="round" type="primary" />
+    </Space>
+  ),
+};
+
+export const ButtonSizes: Story = {
+  name: "按钮尺寸",
+  render: (args) => (
+    <Space size="large" align="start">
+      <Button {...args} size="mini" type="primary">
+        Mini
+      </Button>
+      <Button {...args} size="small" type="primary">
+        Small
+      </Button>
+      <Button {...args} size="default" type="primary">
+        Default
+      </Button>
+      <Button {...args} size="large" type="primary">
+        Large
+      </Button>
+    </Space>
+  ),
+};
+
+export const ButtonStatus: Story = {
+  name: "按钮状态",
+  render: (args) => (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 100px)",
+        gridRowGap: 24,
+        gridColumnGap: 24,
+      }}
+    >
+      <Button {...args} type="primary" status="danger">
+        Button Text
+      </Button>
+      <Button {...args} type="dashed" status="danger">
+        Button Text
+      </Button>
+      <Button {...args} type="outline" status="danger">
+        Button Text
+      </Button>
+      <Button {...args} type="text" status="danger">
+        Button Text
+      </Button>
+
+      <Button {...args} type="primary" status="warning">
+        Button Text
+      </Button>
+      <Button {...args} type="dashed" status="warning">
+        Button Text
+      </Button>
+      <Button {...args} type="outline" status="warning">
+        Button Text
+      </Button>
+      <Button {...args} type="text" status="warning">
+        Button Text
+      </Button>
+
+      <Button {...args} type="primary" status="success">
+        Button Text
+      </Button>
+      <Button {...args} type="dashed" status="success">
+        Button Text
+      </Button>
+      <Button {...args} type="outline" status="success">
+        Button Text
+      </Button>
+      <Button {...args} type="text" status="success">
+        Button Text
+      </Button>
+
+      <Button {...args} type="primary" status="primary">
+        Button Text
+      </Button>
+      <Button {...args} type="dashed" status="primary">
+        Button Text
+      </Button>
+      <Button {...args} type="outline" status="primary">
+        Button Text
+      </Button>
+      <Button {...args} type="text" status="primary">
+        Button Text
+      </Button>
+    </div>
+  ),
+};
+
+export const DisabledButtons: Story = {
+  name: "禁用状态",
+  render: (args) => (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 100px)",
+        gridRowGap: 24,
+        gridColumnGap: 24,
+      }}
+    >
+      <Button {...args} disabled type="primary" status="danger">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="dashed" status="danger">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="outline" status="danger">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="text" status="danger">
+        Button Text
+      </Button>
+
+      <Button {...args} disabled type="primary" status="warning">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="dashed" status="warning">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="outline" status="warning">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="text" status="warning">
+        Button Text
+      </Button>
+
+      <Button {...args} disabled type="primary" status="success">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="dashed" status="success">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="outline" status="success">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="text" status="success">
+        Button Text
+      </Button>
+
+      <Button {...args} disabled type="primary" status="primary">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="dashed" status="primary">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="outline" status="primary">
+        Button Text
+      </Button>
+      <Button {...args} disabled type="text" status="primary">
+        Button Text
+      </Button>
+    </div>
+  ),
+};
+
+const ButtonGroupsTemplate = (props: { disabled?: boolean }) => {
+  const { disabled = false } = props;
+  return (
+    <Space size="large">
+      <Button.Group>
+        <Button disabled={disabled} type="primary">
+          Button Text
+        </Button>
+        <Button disabled={disabled} type="primary" icon={<IconDown />} />
+      </Button.Group>
+      <Button.Group>
+        <Button disabled={disabled}>Button Text</Button>
+        <Button disabled={disabled} icon={<IconDown />} />
+      </Button.Group>
+      <Button.Group>
+        <Button type="primary" icon={<IconLeft />} shape="round">
+          Prev
+        </Button>
+        <Button type="primary" disabled={disabled} shape="round">
+          Next
+          <IconRight />
+        </Button>
+      </Button.Group>
+      <Button.Group>
+        <Button type="primary" icon={<IconAlignLeft />} />
+        <Button type="primary" disabled={disabled} icon={<IconAlignCenter />} />
+        <Button type="primary" icon={<IconAlignRight />} />
+      </Button.Group>
+      <Button.Group>
+        <Button type="primary" icon={<IconStar />}>
+          喜欢
+        </Button>
+        <Button type="primary" disabled={disabled} icon={<IconSettings />}>
+          设置
+        </Button>
+      </Button.Group>
+    </Space>
+  );
+};
+
+export const ButtonGroups: Story = {
+  name: "组合按钮",
+  render: () => (
+    <Space size="large" direction="vertical">
+      <ButtonGroupsTemplate />
+      <ButtonGroupsTemplate disabled />
+    </Space>
+  ),
+};
+
+export const LongButtons: Story = {
+  name: "长按钮",
+  render: () => (
+    <Space
+      style={{
+        width: 360,
+        border: "1px solid var(--color-border)",
+        borderRadius: 4,
+        padding: 20,
+      }}
+      direction="vertical"
+      size="large"
+    >
+      <Button type="primary" long>
+        主要按钮
+      </Button>
+      <Button type="secondary" long>
+        默认按钮
+      </Button>
+      <Button type="dashed" long>
+        虚线按钮
+      </Button>
+      <Button type="outline" long>
+        线框按钮
+      </Button>
+      <Button type="text" long>
+        文字按钮
+      </Button>
+    </Space>
+  ),
+};
