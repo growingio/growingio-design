19579c3fe8ade32eac36ffdaa54a6db0cc9028fa
Pan Jie <fenqi@startdt.com>
2023-12-14 16:19:59 +0800

    feat(input-number): setup input number component

---
+++ packages/base/src/input-number/index.ts
@@ -0,0 +1,4 @@
+import InputNumber from '@arco-design/web-react/es/InputNumber';
+
+export type { InputNumberProps } from '@arco-design/web-react/es/InputNumber';
+export default InputNumber;
---
+++ packages/base/src/input-number/style/index.ts
@@ -0,0 +1,2 @@
+import '@arco-design/web-react/es/InputNumber/style/index.css';
+import './index.css';
