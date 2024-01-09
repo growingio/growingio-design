81f311ff21acb600e3d4ac9b9586dbd99d559f8d
Pan Jie <fenqi@startdt.com>
2024-01-09 19:50:49 +0800

    feat(icon): add color icons and emojis

--- apps/website/stories/icons/IconPanel.tsx
+++ apps/website/stories/icons/IconPanel.tsx
@@ -13,6 +13,8 @@ import {
   EDIT_ICONS,
   MEDIA_ICONS,
   FILLED_ICONS,
+  COLOR_ICONS,
+  EMOJIS,
 } from './Icons';
 
 const renderIcons = (icons: string[], title: string) => (
@@ -37,12 +39,12 @@ const renderIcons = (icons: string[], title: string) => (
     <div style={{ padding: '12px 12px 0px 12px' }}>
       <IconGallery>
         {icons.map((name) => {
-          const iconElement = `Icon${name}`;
+          const iconElement = name.startsWith('Icon') ? name : `Icon${name}`;
           if (!Icon[iconElement]) {
             console.info('[*] icon: ', name);
           }
           return (
-            <IconItem key={name} name={name}>
+            <IconItem key={name} name={name.replace('Icon', '')}>
               {React.createElement(Icon[iconElement])}
             </IconItem>
           );
@@ -57,9 +59,35 @@ export default function () {
   const { formatMessage } = useIntl();
 
   const content = () => {
+    if (iconType === 'emoji') {
+      return (
+        <>
+          {renderIcons(
+            EMOJIS,
+            formatMessage(
+              { defaultMessage: 'Emoji({count})' },
+              { count: EMOJIS.length },
+            ),
+          )}
+        </>
+      );
+    }
+    if (iconType === 'color') {
+      return (
+        <>
+          {renderIcons(
+            COLOR_ICONS,
+            formatMessage(
+              { defaultMessage: '多色图标（{count}）' },
+              { count: COLOR_ICONS.length },
+            ),
+          )}
+        </>
+      );
+    }
     if (iconType === 'filled') {
       return (
-        <div>
+        <>
           {renderIcons(
             FILLED_ICONS,
             formatMessage(
@@ -67,84 +95,68 @@ export default function () {
               { count: FILLED_ICONS.length },
             ),
           )}
-        </div>
+        </>
       );
     }
     return (
-      <div>
-        <div>
-          {renderIcons(
-            GENERAL_ICONS,
-            formatMessage(
-              { defaultMessage: '通用类图标（{count}）' },
-              { count: GENERAL_ICONS.length },
-            ),
-          )}
-        </div>
-        <div>
-          {renderIcons(
-            USER_ICONS,
-            formatMessage(
-              { defaultMessage: '用户图标（{count}）' },
-              { count: USER_ICONS.length },
-            ),
-          )}
-        </div>
-        <div>
-          {renderIcons(
-            DATA_ICONS,
-            formatMessage(
-              { defaultMessage: '数据、业务图标（{count}）' },
-              { count: DATA_ICONS.length },
-            ),
-          )}
-        </div>
-        <div>
-          {renderIcons(
-            DIRECTION_ICONS,
-            formatMessage(
-              { defaultMessage: '方向指示图标（{count}）' },
-              { count: DIRECTION_ICONS.length },
-            ),
-          )}
-        </div>
-        <div>
-          {renderIcons(
-            TIPS_ICONS,
-            formatMessage(
-              { defaultMessage: '提示建议类图标（{count}）' },
-              { count: TIPS_ICONS.length },
-            ),
-          )}
-        </div>
-        <div>
-          {renderIcons(
-            PLATFORM_DEVICE_ICONS,
-            formatMessage(
-              { defaultMessage: '平台和设备图标（{count}）' },
-              { count: PLATFORM_DEVICE_ICONS.length },
-            ),
-          )}
-        </div>
-        <div>
-          {renderIcons(
-            EDIT_ICONS,
-            formatMessage(
-              { defaultMessage: '编辑类图标（{count}）' },
-              { count: EDIT_ICONS.length },
-            ),
-          )}
-        </div>
-        <div>
-          {renderIcons(
-            MEDIA_ICONS,
-            formatMessage(
-              { defaultMessage: '影音类图标（{count}）' },
-              { count: MEDIA_ICONS.length },
-            ),
-          )}
-        </div>
-      </div>
+      <>
+        {renderIcons(
+          GENERAL_ICONS,
+          formatMessage(
+            { defaultMessage: '通用类图标（{count}）' },
+            { count: GENERAL_ICONS.length },
+          ),
+        )}
+        {renderIcons(
+          USER_ICONS,
+          formatMessage(
+            { defaultMessage: '用户图标（{count}）' },
+            { count: USER_ICONS.length },
+          ),
+        )}
+        {renderIcons(
+          DATA_ICONS,
+          formatMessage(
+            { defaultMessage: '数据、业务图标（{count}）' },
+            { count: DATA_ICONS.length },
+          ),
+        )}
+        {renderIcons(
+          DIRECTION_ICONS,
+          formatMessage(
+            { defaultMessage: '方向指示图标（{count}）' },
+            { count: DIRECTION_ICONS.length },
+          ),
+        )}
+        {renderIcons(
+          TIPS_ICONS,
+          formatMessage(
+            { defaultMessage: '提示建议类图标（{count}）' },
+            { count: TIPS_ICONS.length },
+          ),
+        )}
+        {renderIcons(
+          PLATFORM_DEVICE_ICONS,
+          formatMessage(
+            { defaultMessage: '平台和设备图标（{count}）' },
+            { count: PLATFORM_DEVICE_ICONS.length },
+          ),
+        )}
+        {renderIcons(
+          EDIT_ICONS,
+          formatMessage(
+            { defaultMessage: '编辑类图标（{count}）' },
+            { count: EDIT_ICONS.length },
+          ),
+        )}
+        {renderIcons(
+          MEDIA_ICONS,
+          formatMessage(
+            { defaultMessage: '影音类图标（{count}）' },
+            { count: MEDIA_ICONS.length },
+          ),
+        )}
+      </>
     );
   };
   return (
@@ -156,6 +168,12 @@ export default function () {
           </Radio>
           <Radio value="filled">
             <FormattedMessage defaultMessage="面性图标" />
+          </Radio>
+          <Radio value="color">
+            <FormattedMessage defaultMessage="多色图标" />
+          </Radio>
+          <Radio value="emoji">
+            <FormattedMessage defaultMessage="Emoji" />
           </Radio>
         </Radio.Group>
       </div>
