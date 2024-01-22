2c07fcddbf0bfeb5880679ba406cec1c4fdf1254
Pan Jie <fenqi@startdt.com>
2024-01-22 12:01:48 +0800

    feat: setup verification code component

---
+++ packages/base/src/verification-code/index.ts
@@ -0,0 +1,5 @@
+import VerificationCode from '@arco-design/web-react/es/VerificationCode';
+
+export type { VerificationCodeProps } from '@arco-design/web-react/es/VerificationCode';
+export { default as useVerificationCode } from '@arco-design/web-react/es/_hooks/useVerificationCode';
+export default VerificationCode;
